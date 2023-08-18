12463ddad7d9a3e41170a5c84f78acff3129f23b
fancilivain <meigangwen@gmail.com>
2023-08-18 09:30:08 +0800

    unable to make demo6 work

---
+++ app/demo6/page.tsx
@@ -0,0 +1,96 @@
+'use client'
+
+import { useEffect} from "react"
+import vertexShaderSource from './shaders/vertex.glsl'
+import fragmentShaderSource from './shaders/fragment.glsl'
+import * as Render from './render'
+//import m3 from './m3'
+
+export default function Home() {
+  
+  useEffect(() => {
+  
+    function main() {
+      var canvas = document.querySelector("#c")
+      var gl = canvas.getContext("webgl2");
+
+      // create GLSL shaders, upload the GLSL source, compile the shaders
+      var vertexShader = Render.createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+      var fragmentShader = Render.createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+      var program = Render.createProgram(gl, vertexShader, fragmentShader);
+
+      // make some variables to hold the translation data
+      var translation = [0,0]
+      var width = 100
+      var height = 30
+      var color = [Math.random(), Math.random(), Math.random(), 1]
+
+
+      // look up where the vertex data needs to go.
+      var positionLocation = gl.getAttribLocation(program, "a_position");
+      var colorLocation = gl.getAttribLocation(program, "u_color");
+      var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+
+      // Create a buffer and put three 2d clip space points in it
+      var positionBuffer = gl.createBuffer();
+
+      // Create a vertex array object (attribute state)
+      var vao = gl.createVertexArray()
+      gl.bindVertexArray(vao)
+
+      // setup attributes
+      gl.enableVertexAttribArray(positionLocation)
+      gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+      var size = 2;          // 2 components per iteration
+      var type = gl.FLOAT;   // the data is 32bit floats
+      var normalize = false; // don't normalize the data
+      var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+      var offset = 0;        // start at the beginning of the buffer
+      gl.vertexAttribPointer(positionLocation, size, type, normalize, stride, offset)
+
+      drawScene()
+
+      function drawScene(){
+        Render.resizeCanvasToDisplaySize(gl.canvas)
+        
+        // Tell WebGL how to convert from clip space to pixels
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+
+        // Clear the canvas
+        gl.clearColor(0, 0, 0, 0)
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+        // Tell it to use our program (pair of shaders)
+        gl.useProgram(program);
+
+        // Bind the attribute/buffer set we want.
+        gl.bindVertexArray(vao);
+
+        // Pass in the canvas resolution so we can convert from
+        // pixels to clip space in the shader
+        gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height)
+
+        // Update the position buffer with rectangle positions
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+        Render.setRectangle(gl, translation[0],translation[1], width, height)
+
+        // Set the color
+        gl.uniform4fv(colorLocation, color)
+        
+        // Draw the rectangle
+        var primitiveType = gl.TRIANGLES;
+        var offset = 0
+        var count = 6
+        gl.drawArrays(primitiveType, offset, count);
+
+      }
+    }
+    main()
+  },[])
+
+  return (
+    <>
+      <canvas id="c"></canvas>
+    </>
+  )
+}
---
+++ app/demo6/render.tsx
@@ -0,0 +1,110 @@
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type)
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader)
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+    if (success) {
+    return shader
+    }
+    
+    console.log(gl.getShaderInfoLog(shader))
+    gl.deleteShader(shader)
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+    return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));  // eslint-disable-line
+    gl.deleteProgram(program);
+    return undefined;
+}
+
+function resizeCanvasToDisplaySize(canvas) {
+  // Lookup the size the browser is displaying the canvas in CSS pixels.
+  const displayWidth  = canvas.clientWidth;
+  const displayHeight = canvas.clientHeight;
+ 
+  // Check if the canvas is not the same size.
+  const needResize = canvas.width  !== displayWidth ||
+                     canvas.height !== displayHeight;
+ 
+  if (needResize) {
+    // Make the canvas the same size
+    canvas.width  = displayWidth;
+    canvas.height = displayHeight;
+  }
+ 
+  return needResize;
+}
+
+// Returns a random integer from 0 to range - 1.
+function randomInt(range) {
+  return Math.floor(Math.random() * range);
+}
+
+// Fill the buffer with the values that define a rectangle.
+function setRectangle(gl,x,y,width,height) {
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+     x1, y1,
+     x2, y1,
+     x1, y2,
+     x1, y2,
+     x2, y1,
+     x2, y2,
+  ]), gl.STATIC_DRAW);
+}
+
+// Set a very specific triangle
+function setTriangle(gl) {
+  gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array([
+             0, -100,
+           150,  125,
+          -175,  100]),
+      gl.STATIC_DRAW);
+}
+
+function setColors(gl) {
+  // Pick 2 random colors.
+  var r1 = Math.random();
+  var b1 = Math.random();
+  var g1 = Math.random();
+ 
+  var r2 = Math.random();
+  var b2 = Math.random();
+  var g2 = Math.random();
+ 
+  gl.bufferData(
+      gl.ARRAY_BUFFER,
+      /*
+      new Float32Array(
+        [ r1, b1, g1, 1,
+          r1, b1, g1, 1,
+          r1, b1, g1, 1,
+          r2, b2, g2, 1,
+          r2, b2, g2, 1,
+          r2, b2, g2, 1]),
+      */
+      new Float32Array(
+        [ Math.random(), Math.random(), Math.random(), 1,
+          Math.random(), Math.random(), Math.random(), 1,
+          Math.random(), Math.random(), Math.random(), 1,
+          Math.random(), Math.random(), Math.random(), 1,
+          Math.random(), Math.random(), Math.random(), 1,
+          Math.random(), Math.random(), Math.random(), 1]),
+      gl.STATIC_DRAW);
+}
+
+export {createShader, createProgram, resizeCanvasToDisplaySize, randomInt, setRectangle, setTriangle, setColors}
